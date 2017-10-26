17a3bf5e899d57c9815382f28705b0179f7e2335
sayo.arakawa <arakawa@consulting-r.biz>
2017-10-26 06:14:10 +0000

    chart model

---
+++ database/migrations/2017_10_26_150100_create_charts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('charts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sentence');
+            $table->integer('yes_id');
+            $table->integer('no_id');
+            $table->integer('yes_result_id');
+            $table->integer('no_result_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('charts');
+    }
+}
